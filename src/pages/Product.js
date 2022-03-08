33c9271061d43d2ef3105011d9ff059127a97808
ameliadwijayani <57041004+ameliadwijayani@users.noreply.github.com>
2022-03-08 21:29:59 +0700

    Update Product.js

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -22,7 +22,8 @@ export default function Product({data}) {
             <div className="project-item filterable-item shopping-center">
               <figure className="featured-image">
                 <a href="project-single.html">
-                  <img src={node.frontmatter.images?.childImageSharp.gatsbyImageData.images.fallback.src }alt="#" />
+                  <img src={node.frontmatter.Images?.childImageSharp.gatsbyImageData.images.fallback.src }alt="" />
+                  <img src={node.frontmatter.images?.childImageSharp.gatsbyImageData.images.fallback.src }alt="" />
                   </a>
                 <figcaption>
                   <h2 className="project-title">{node.frontmatter.title}</h2>
