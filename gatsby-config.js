629280156085776325a428397587d7e917067e6b
ameliadwijayani <57041004+ameliadwijayani@users.noreply.github.com>
2022-03-08 16:47:01 +0700

    b

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
     siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
+        
+    title: `Samijaya Probolinggo`,
+    description: `Samijaya Probolinggo adalah toko yang menjual berbagai peralatan elektronik mulai dari lampu, kulkas, tv, blender bahkan HP. Kami berdiri pada tahun 2008 di Kota Probolinggo dan telah menjual produk-produk andalan yang telah digunakan di seluruh pelosok tanah air. Terima kasih telah mengunjungi website kami dan atas kepercayaan anda dalam memilih produk-produk kami.`,
+    author: `@samijayaprobolinggo`,
     },
     plugins: [
  
