e6a1086ab6676a223adea3f1761b8c306349b6bc
ameliadwijayani <57041004+ameliadwijayani@users.noreply.github.com>
2022-06-11 01:08:09 +0700

    a

--- src/pages/Layout/Layout.js
+++ src/pages/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import SEO from '../Component/Seo.js'
-import Footer from "./Footer.js"
-import Header from "./Header.js"
+import SEO from '../Component/Seo'
+import Footer from "./Footer"
+import Header from "./Header"
 import "../Styles.css"
 export default function Layout({children, description, title}) {
   return (
